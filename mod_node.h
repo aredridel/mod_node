d274c902db24e3d9ff4c09836514d205e9ccba29
Aria Stewart <aredridel@nbtsc.org>
2011-09-17 11:05:47 -0400

    A first pass at reimplementing as a simple binding.
    Not working yet.

--- mod_node.h
+++ mod_node.h
@@ -2,7 +2,7 @@
 #include <node.h>
 
 namespace mod_node {
-	process_rec *process;
-	apr_thread_t *thread;
-	extern void Initialize(v8::Handle<v8::Object> target);
+    process_rec *process;
+    apr_thread_t *thread;
+    extern void Initialize(v8::Handle<v8::Object> target);
 }
--- runtime.js
+++ runtime.js
@@ -2,10 +2,13 @@ var apache = require('apache');
 var fs = require('fs');
 var util = require('util');
 
+console.log(apache.process);
+
 apache.process.critical("critical");
 apache.process.warn("warn");
 apache.process.log("info");
 
+/*
 apache.process.on("connection", function(req) {
 	apache.process.warn("Got a connection")
 	apache.process.write("Hi mom!");
@@ -14,3 +17,9 @@ apache.process.on("connection", function(req) {
 process.on('exit', function() {
 	apache.process.warn('shutting down node')
 })
+
+*/
+
+apache.process.onrequest = function(req) {
+    console.log(req)
+}
